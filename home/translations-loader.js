56fa92888dde7ad8c0f9db781316e7a243a50822
rex09380931-blip <r.ex09380931@gmail.com>
2026-08-01 13:55:11 +0400

    Delete home directory

--- home/translations-loader.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  window.REX_TRANSLATIONS = window.REX_TRANSLATIONS || { en: {}, ar: {} };
-  Object.assign(window.REX_TRANSLATIONS.en, {
-  "loader.entering-r-ex": "ENTERING R-EX"
-});
-  Object.assign(window.REX_TRANSLATIONS.ar, {
-  "loader.entering-r-ex": "جارٍ الدخول إلى R-EX"
-});
-})();
